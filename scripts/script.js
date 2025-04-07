70aa1f8efa5bd4205705562f36fa104bddbaa6d5
Starfoxxy <athenajacob97@gmail.com>
2025-04-07 03:21:21 -0400

    The querySelector() method returns the first element that matches the specified CSS selector. In index.html, I was able to use both id and class, while also using different tags. In script.js, I created variables for all of these and querySelector selects these specific elements. We can see these changes using console.log().

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
 let titleElement = document.getElementById("title");
 
 console.log(titleElement);
+
+
+let candy = document.querySelector("h2");
+let paragraph = document.querySelector("p");
+let click = document.querySelector("button");
+
+console.log(candy);
+console.log(paragraph);
+console.log(click);
